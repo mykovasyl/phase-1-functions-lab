d9218f64d07a6f1109fdc9744638849cf3a2cfe4
Mykola Vasyl'yev <mykovasyl@gmail.com>
2021-11-07 15:54:21 -0500

    commit tree f89636fd2575a88f64b83d36d15fce0ca15d8b2e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42 - pickupLocation);
+}
+
+function distanceFromHqInFeet(distFromHq) {
+    return Math.abs(42 - distFromHq) * 264;
+}
+
+function distanceTravelledInFeet(hqLocation, pickupLocation) {
+    return Math.abs(hqLocation - pickupLocation) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let rideDistance = Math.abs(start - destination) * 264;
+    if ((rideDistance < 400)) {
+        return 0;
+    } else if ((rideDistance >= 400 && rideDistance <= 2000)) {
+        return (rideDistance - 400) * 0.02;
+    } else if ((rideDistance > 2000 && rideDistance <= 2500)) {
+        return 25;
+    } else {
+        return `cannot travel that far`;
+    }
+}
